a1f631b6839f6882ed29dd9b739518be747162c6
Pavan Navde <pavannavde1997@gmail.com>
2024-05-06 12:57:25 +0530

    13

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(cors({
     origin:["https://customer-application-frontend.vercel.app"],
     methods: ["GET", "POST"],
-    credentials: true,
+    credentials: true
 }));
 app.use(express.urlencoded({ extended: true }));
 app.use(
